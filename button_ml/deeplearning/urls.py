c0f50f6710b5afee955b41beaf63aa06cc5f89b4
김지수 <jisookim@gimjisuui-MacBookPro.local>
2020-11-17 00:09:32 +0900

    change url, view

--- button_ml/deeplearning/urls.py
+++ button_ml/deeplearning/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import post_cloth, modify_cloth, delete_cloth, get_set
+from .views import post_cloth, get_set
+#modify_cloth, delete_cloth,
 from . import views
 # , change_cloth, delete_cloth, get_set
 
 urlpatterns = [
     path('postCloth/', views.post_cloth),
-    path('changeCloth/', views.modify_cloth),
-    path('deleteCloth/', views.delete_cloth),
+    # path('changeCloth/', views.modify_cloth),
+    # path('deleteCloth/', views.delete_cloth),
     path('getSet/', views.get_set),
 ]
--- button_ml/deeplearning/views.py
+++ button_ml/deeplearning/views.py
@@ -12,7 +12,8 @@ from rest_framework.views import APIView
 from . import polyvore
 from .polyvore import run_inference, set_generation
 from .polyvore.set_generation import set_generation
-from .polyvore.run_inference import extract_features, delete_extract_features, modify_extract_features
+from .polyvore.run_inference import extract_features
+#delete_extract_features, modify_extract_features
 from rest_framework.decorators import api_view, permission_classes
 from .serializers import Cloth_SSerializer
 import json
@@ -46,35 +47,35 @@ def post_cloth(request):
 #     pass
 
 
-@api_view(['POST'])
-def delete_cloth(request):
-    print(request.data)
-    # data = request.data.get("data")
-    # data["photo"] = request.data.get("photo")
-    # print(data["photo"])
-    decodedSet = jsonpickle.decode(request.data)
-    print(decodedSet)
-    print("start del extract")
-    delete_extract_features(decodedSet)
-    print("doen del extract")
+# @api_view(['POST'])
+# def delete_cloth(request):
+#     print(request.data)
+#     # data = request.data.get("data")
+#     # data["photo"] = request.data.get("photo")
+#     # print(data["photo"])
+#     decodedSet = jsonpickle.decode(request.data)
+#     print(decodedSet)
+#     print("start del extract")
+#     delete_extract_features(decodedSet)
+#     print("doen del extract")
 
-    # delete_extract_features(request.data)
-    return Response({'response': 'done'})
+#     # delete_extract_features(request.data)
+#     return Response({'response': 'done'})
 
 
-@api_view(['POST'])
-def modify_cloth(request):
-    print(request.data)
-    # data = request.data.get("data")
-    # data["photo"] = request.data.get("photo")
-    decodedSet = jsonpickle.decode(request.data)
-    print(decodedSet)
-    print("start mod extract")
-    modify_extract_features(decodedSet)
-    # delete_extract_features(decodedSet)
-    print("doen mod extract")
-    # modify_extract_features(request.data)
-    return Response({'response': 'done'})
+# @api_view(['POST'])
+# def modify_cloth(request):
+#     print(request.data)
+#     # data = request.data.get("data")
+#     # data["photo"] = request.data.get("photo")
+#     decodedSet = jsonpickle.decode(request.data)
+#     print(decodedSet)
+#     print("start mod extract")
+#     modify_extract_features(decodedSet)
+#     # delete_extract_features(decodedSet)
+#     print("doen mod extract")
+#     # modify_extract_features(request.data)
+#     return Response({'response': 'done'})
 
 
 @api_view(['POST'])
